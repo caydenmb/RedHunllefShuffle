13872f730a9a42c091af18453a2a640f8e6c1b0a
Cayden B. <83572326+caydenmb@users.noreply.github.com>
2025-04-23 01:50:15 -0600

    Update chknrace.py

--- chknrace.py
+++ chknrace.py
@@ -19,8 +19,8 @@ CORS(app)  # Allow JS on the frontend to fetch /data
 # --- Configuration ---------------------------------------------------------
 
 API_KEY    = "f45f746d-b021-494d-b9b6-b47628ee5cc9"
-START_TIME = 1745371655         # April 22, 2025 21:27:35 EDT
-END_TIME   = 1746849540         # May 9, 2025 23:59:00 EDT → UTC 2025-05-10 03:59:00
+START_TIME = 1745334000
+END_TIME   = 1746849540
 
 URL_TEMPLATE = (
     f"https://affiliate.shuffle.com/stats/{API_KEY}"
